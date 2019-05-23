482ceacec2c93a521139c91281374eda89bde491
Md Mahmood Bin Habib <habib@kutumbita.com>
2019-05-23 14:59:04 +0600

    student models created

---
+++ backend/student_management/api/models.py
@@ -0,0 +1,14 @@
+from google.appengine.ext import ndb
+
+
+class Student(ndb.Model):
+    id = ndb.IntegerProperty()
+    roll = ndb.IntegerProperty()
+    name = ndb.StringProperty()
+    student_class = ndb.InterProperty()
+    date_of_birth = ndb.DateProperty()
+    address = ndb.StringProperty()
+    phone_number = ndb.StringProperty()
+    email_address = ndb.EmailProperty()
+    created_at = ndb.DateTimeProperty(auto_now_add=True)
+    updated_at = ndb.DateTimeProperty()
